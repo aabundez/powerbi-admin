e9ab18e05ec15f18dd7d9e50f962f737f9dc04b5
Angel Abundez <angel@designmind.com>
2019-06-12 17:39:32 -0700

    Enhancements for Sunnyvale UG 6/12/19

--- PBI_Inventory.ps1
+++ PBI_Inventory.ps1
@@ -1,35 +1,54 @@
 ﻿<#
 
 .SYNOPSIS
- Collect Power BI assets across a tenant.
+ Collect Power BI assets across a tenant. 
+ Must have Power BI Administration or Global Administrator role in O365.
 
 .DESCRIPTION
- The script relies on multiple API end points from the Power BI Rest API. 
- For more information, reference Microsoft docs: https://docs.microsoft.com/en-us/rest/api/power-bi/
+ The script relies on Power BI REST API and Power BI Powershell module. 
+ For more information, reference Microsoft docs: 
+ https://docs.microsoft.com/en-us/rest/api/power-bi/
+ https://docs.microsoft.com/en-us/powershell/power-bi/overview?view=powerbi-ps
 
 .EXAMPLE
  ./PBI_Inventory.ps1
 
 .LINK
  https://docs.microsoft.com/en-us/rest/api/power-bi/
+ https://docs.microsoft.com/en-us/powershell/power-bi/overview?view=powerbi-ps
 
 #>
 
-#Connect to the Power BI Service
+#Install Power BI Module
+$moduleinstalled = Find-Module -Name MicrosoftPowerBIMgmt
+if (!$moduleInstalled){
+    Install-Module -Name MicrosoftPowerBIMgmt
+}
+else{
+    Write-Host "Power BI PS Module Installed!"
+}
+
+#Get Headers
 try {
-    Get-PowerBIAccessToken 
+    $authtoken = Get-PowerBIAccessToken
 }
 catch {
+    Write-Host "Have not logged in yet. Initiating login..."
     Login-PowerBIServiceAccount
 }
 
-#Get Power BI Workspaces
-$ws = Get-PowerBIWorkspace -Scope Organization -Top 2000
-$ws | Export-Csv -Path "C:\temp\workspaces.csv" -NoTypeInformation
+#Set Output Path
+$path = "C:\tempug"
 
+#Get Power BI Workspaces
+Write-Host "Exporting Power BI Workspaces..."
+$results = Invoke-RestMethod -Headers $authtoken -Uri "https://api.powerbi.com/v1.0/myorg/admin/groups?`$top=100&`$expand=users" -Method Get
+$ws = $results.value
+# Export Power BI Workspaces 
+$ws | Export-Csv -Path "$path\workspaces.csv" -NoTypeInformation
 
 #Get Power BI Datasets
-
+Write-Host "Exporting Power BI Datasets..."
 #Initiate PSCustomObject to collect Datasets
 $refs = @()
 $i = 0
@@ -43,8 +62,9 @@ foreach ($w in $ws) {
 
     Write-Host $i. "Workspace.id = " $wid " workspace.name = " $wname
 
-    #Get datasets for workspace. If empty, continue to next workspace.
-    $ds = Get-PowerBIDataset -WorkspaceId $wid -Scope Organization -ErrorAction SilentlyContinue 
+    # Base dataset of Datasets
+    $results = Invoke-RestMethod -Headers $authtoken -Uri "https://api.powerbi.com/v1.0/myorg/admin/groups/$wid/datasets" -Method Get
+    $ds = $results.value
     if(!$ds){continue}
 
     #Add Workspace information
@@ -53,16 +73,14 @@ foreach ($w in $ws) {
 
     #Append to final object
     $refs += $ds
+}
 
-
-   }
-
-$refs | Export-Csv -Path "C:\temp\datasets.csv" -NoTypeInformation
+$refs | Export-Csv -Path "$path\datasets.csv" -NoTypeInformation
 
 # Get Power BI Reports
-
+Write-Host "Exporting Power BI Reports..."
 $rp = Get-PowerBIReport -Scope Organization
-$rp | Export-Csv -Path "C:\temp\reports.csv" -NoTypeInformation
+$rp | Export-Csv -Path "$path\reports.csv" -NoTypeInformation
 
 
 Logout-PowerBIServiceAccount
