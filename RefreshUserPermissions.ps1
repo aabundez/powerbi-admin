dedb4a53c4ba6c0a3f2e4cb11dc86870d9da24fb
Angel Abundez <angel@designmind.com>
2019-08-19 17:35:32 -0700

    Create RefreshUserPermissions.ps1

---
+++ RefreshUserPermissions.ps1
@@ -0,0 +1,25 @@
+﻿<#
+
+.SYNOPSIS
+Refresh user permissions.
+
+.DESCRIPTION
+ Refresh user permissions API call. See https://docs.microsoft.com/en-us/rest/api/power-bi/users/refreshuserpermissions
+
+.EXAMPLE
+./RefreshUserPermissions.ps1
+
+.LINK
+http://www.angelabundez.com
+
+#>
+
+Login-PowerBI
+
+$token = Get-PowerBIAccessToken
+
+$url = "https://api.powerbi.com/v1.0/myorg/RefreshUserPermissions"
+
+Invoke-RestMethod -Headers $token -Uri $url -Method Post
+
+Resolve-PowerBIError -Last
